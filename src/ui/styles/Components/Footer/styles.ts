4ef59e81e80146fe5e1f428830bab1a08b48c46a
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2024-10-24 18:34:43 -0300

    Refatorar estilos do componente Footer para remover estilos não utilizados

--- src/ui/styles/Components/Footer/styles.ts
+++ src/ui/styles/Components/Footer/styles.ts
@@ -5,9 +5,7 @@ export const EstiloRodape = styled.footer`
   padding: 15px;
   background-color: #282c34;
   color: white;
-  position: absolute;
   width: 100%;
-  bottom: 0;
 `;
 
 export const TextoRodape = styled.p`
