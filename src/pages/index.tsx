d94fb62e08400352fe2315e3ba7d85ac09300200
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2024-10-24 16:24:00 -0300

    Add Quadrado, Titulo, and Botao components with styled styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import SEO from "@/components/SEO";
 import { HomePageTextsSEO } from "@/components/SEO/seoTexts";
+import Quadrado from "@/components/Quadrado";
+import Titulo from "@/components/Titulo";
+import { Container } from "@/ui/styles/Pages/home/styles";
+import Botao from "@/components/Botao";
 
 export default function Home() {
   const seoText = HomePageTextsSEO;
 
   return (
-    <div>
+    <Container>
       <SEO
         title={seoText.title}
         description={seoText.description}
@@ -15,8 +19,18 @@ export default function Home() {
         url={seoText.url}
         image={seoText.image}
       />
-
-      <h1>home page</h1>
-    </div>
+      <div>
+        <h4>Componente quadrado</h4>
+        <Quadrado cor="blue" tamanho={100} />
+      </div>
+      <div>
+        <h4>Componente de Título</h4>
+        <Titulo texto="Título de exemplo" />
+      </div>
+      <div>
+        <h4>Componente de Título</h4>
+        <Botao texto="Clique aqui" onClick={() => alert("Botão clicado!")} />
+      </div>
+    </Container>
   );
 }
--- src/ui/styles/Pages/home/styles.ts
+++ src/ui/styles/Pages/home/styles.ts
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  /* exemplos: */
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100vh;
   background-color: ${({ theme }) => theme.colors.backgrounds.secondary};
   color: ${({ theme }) => theme.colors.text.primary};
+  flex-wrap: wrap;
+  gap: 2rem;
 `;
