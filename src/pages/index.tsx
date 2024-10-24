167514c4c018ce89e20c6122747b136008e85274
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2024-10-24 17:47:20 -0300

    Refatorar estilos do componente Titulo para utilizar estilos personalizados

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,8 @@ import Titulo from "@/components/Titulo";
 import { Container } from "@/ui/styles/Pages/home/styles";
 import Botao from "@/components/Botao";
 import ListaItens from "@/components/ListaItens";
+import Imagem from "@/components/Imagem";
+import Paragrafo from "@/components/Paragrafo";
 
 export default function Home() {
   const seoText = HomePageTextsSEO;
@@ -23,7 +25,7 @@ export default function Home() {
       />
       <div>
         <h4>Componente quadrado</h4>
-        <Quadrado cor="blue" tamanho={100} />
+        <Quadrado cor="blue" tamanho="100px" />
       </div>
 
       <div>
@@ -39,6 +41,19 @@ export default function Home() {
       <div>
         <h4>Componente de Lista</h4>
         <ListaItens itens={itens} />
+      </div>
+
+      <div>
+        <h4>Componente de imagem</h4>
+        <Imagem
+          url="https://avatars.githubusercontent.com/u/67611716?v=4"
+          alt="Imagem de exemplo"
+        />
+      </div>
+
+      <div>
+        <h4>Componente de Parágrafo</h4>
+        <Paragrafo texto="Exemplo de Parágrafo" />
       </div>
     </Container>
   );
